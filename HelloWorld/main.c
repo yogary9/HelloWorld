d3419f6713b9a87c295777709a60e2feb6a4a9a0
Gary Arakaki <gary@garys-macbook-pro.local>
2018-06-06 22:30:54 +0900

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Gary Arakaki on 6/6/18.
+//  Copyright © 2018 Gary Arakaki. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
